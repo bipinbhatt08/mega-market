fcf9a47bfb609dd3cf89aac03ffe8efee4e12d84
Bipin <bipinbhatt280@gmail.com>
2024-01-23 09:02:54 +0545

    jwt added to the backend and redux providor to the frontend

---
+++ client/src/app/redux/reduxProvider.js
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux';
+import store from './store';
+
+const reduxProvider = ({children})=>{
+    return (
+
+        <Provider store={store}>
+            {children}
+        </Provider>
+    )
+}
+
+export default reduxProvider
+
