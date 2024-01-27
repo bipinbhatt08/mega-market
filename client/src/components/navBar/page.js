6e1088fd622847bfccdf545dc669004adbccda29
Bipin <bipinbhatt280@gmail.com>
2024-01-28 00:17:11 +0545

    auth bottons and user avatar in navbar made dynamic

--- client/src/components/navBar/page.js
+++ client/src/components/navBar/page.js
@@ -1,10 +1,13 @@
 'use client'
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, NavbarMenuToggle,NavbarMenu,NavbarMenuItem} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, NavbarMenuToggle,NavbarMenu,NavbarMenuItem,DropdownItem, DropdownTrigger, Dropdown, DropdownMenu, Avatar} from "@nextui-org/react";
 import { color } from "framer-motion";
 import Link from 'next/link'
+import { useSelector } from "react-redux";
 
 export default function App() {
+
+  const {isLoggedIn}= useSelector(state=>state.user)
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
   const menuItems = [
@@ -19,6 +22,54 @@ export default function App() {
     "Help & Feedback",
     "Log Out",
   ];
+    const UserAvatarDropDown = ()=>{
+      return <>
+      <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="danger"
+              
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger" onClick={()=>{set}}>
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </>
+    }
+    const AuthButtons = ()=>{
+      return <>
+      <NavbarItem className="hidden lg:flex">
+          {/* <Link href="/register" className="loginBtn">Login</Link> */}
+          <Button as={Link}  href="/login"  variant="flat" className="loginBtn">
+            Login
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="/register" variant="flat" className="signUpBtn">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </>
+    }
 
   return (
     <Navbar onMenuOpenChange={setIsMenuOpen} isBordered shouldHideOnScroll>
@@ -47,17 +98,8 @@ export default function App() {
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          {/* <Link href="/register" className="loginBtn">Login</Link> */}
-          <Button as={Link}  href="/login"  variant="flat" className="loginBtn">
-            Login
-          </Button>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="/register" variant="flat" className="signUpBtn">
-            Sign Up
-          </Button>
-        </NavbarItem>
+        {(!isLoggedIn)?<AuthButtons/>:<UserAvatarDropDown/>
+        }
       </NavbarContent>
       <NavbarMenu>
         {menuItems.map((item, index) => (
